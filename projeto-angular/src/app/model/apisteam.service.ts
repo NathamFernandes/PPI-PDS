fa641af795da72f8266606f5496cf9f331186d58
Natham Fernandes <natham.fr@gmail.com>
2022-11-21 00:27:07 -0300

    Services

--- projeto-angular/src/app/model/apisteam.service.ts
+++ projeto-angular/src/app/model/apisteam.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApisteamService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obterStatsUser(iduser: number) : Observable<any> {
+    let URL = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=E9FCC1C5E3BA8368FDABE96C4027CA8D&steamid=${iduser}`
+    return this.http.get(URL)
+  }
+  
+  obterStatsAmigo(iduser: number, idamigo: number) : Observable<any> {
+    let URL_1 = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=E9FCC1C5E3BA8368FDABE96C4027CA8D&steamid=${iduser}`
+    let URL_2 = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=E9FCC1C5E3BA8368FDABE96C4027CA8D&steamid=${idamigo}`
+    return this.http.get(URL_1), this.http.get(URL_2)
+  } 
+
 }

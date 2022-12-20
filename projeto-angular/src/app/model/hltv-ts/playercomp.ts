1b635e3ec2f5328ec0a933267592f61cf10ea96c
Natham Fernandes <natham.fr@gmail.com>
2022-12-20 20:30:35 -0300

    Estilização - COMPEITIVO

--- projeto-angular/src/app/model/hltv-ts/playercomp.ts
+++ projeto-angular/src/app/model/hltv-ts/playercomp.ts
@@ -4,8 +4,14 @@ export class PlayerComp {
     team: TimeParcial = new TimeParcial;
     image: string = '';
     nickname: string = '';
+    name: string = '';
     age: number = 0;
     rating: number = 0;
-    mapsPlayed: number = 0;
+    impact: number = 0;
+    dpr: number = 0;
+    kast: number = 0;
+    kpr: number = 0;
     headshots: number = 0;
+    mapsPlayed: number = 0;
+
 }
